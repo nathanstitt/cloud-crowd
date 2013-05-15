41b18a758a0ece2cc085be85a3dd54782835384b
Nathan Stitt <nathan@stitt.org>
2013-05-15 11:45:23 -0500

    add a clear database helper; auto-migrate test db

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,6 +4,12 @@ require 'rubygems'
 here = File.dirname(__FILE__)
 require File.expand_path(here + "/../lib/cloud-crowd")
 CloudCrowd.configure(here + '/config/config.yml')
+
+unless File.exists?( here+'/cloud_crowd_test.db')
+  CloudCrowd.configure_database("#{here}/config/database.yml", false)
+  require 'cloud_crowd/schema.rb'
+end
+
 CloudCrowd.configure_database(here + '/config/database.yml')
 
 
@@ -17,4 +23,14 @@ require "#{CloudCrowd::ROOT}/test/blueprints.rb"
 
 class Test::Unit::TestCase
   include CloudCrowd
+
+  def clear_database!
+    db = SQLite3::Database.new "#{File.dirname(__FILE__)}/cloud_crowd_test.db"
+    db.execute( "SELECT name FROM sqlite_master WHERE type='table'" ) do | table, unused |
+      unless 'schema_migrations' == table
+        db.execute("delete from #{table}")
+      end
+    end
+  end
+
 end
